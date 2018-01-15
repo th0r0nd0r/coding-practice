0c9ee631070280956844ff7a4659065aaf3df09b
th0r0nd0r <pjfritch1@gmail.com>
2018-01-15 15:06:41 -0800

    shuffle

--- hackerrank/algorithms/01-01.js
+++ hackerrank/algorithms/01-01.js
@@ -14,24 +14,33 @@ function isPalindrome(str) {
   return true;
 }
 
-console.log(isPalindrome("abba")); 
-console.log(isPalindrome("radar"));
-console.log(isPalindrome("abcdba"));
-console.log(isPalindrome("abdca"));
+// console.log(isPalindrome("abba")); 
+// console.log(isPalindrome("radar"));
+// console.log(isPalindrome("abcdba"));
+// console.log(isPalindrome("abdca"));
 
 
 function shuffle(arr) {
+  // console.log("shuffling");
   const len = arr.length;
   const shuffled = [];
   const picked = [];
 
   while (shuffled.length < arr.length) {
     let idx = Math.floor(Math.random() * len);
+    // console.log("idx: ", idx)
     if (!picked.includes(idx)) {
       shuffled.push(arr[idx]);
       picked.push(idx);
+      // console.log("shuffled: ", shuffled);
     }
   }
 
   return shuffled;
 }
+
+console.log(shuffle([1,2,3,4,5]));
+console.log(shuffle([1,2,3,4,5]));
+console.log(shuffle([1,2,3,4,5]));
+console.log(shuffle([1,2,3,4,5]));
+console.log(shuffle([1,2,3,4,5]));
