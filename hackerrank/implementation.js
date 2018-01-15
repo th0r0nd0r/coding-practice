fbd3059e5c704323f6d5d4d19c7c4752ae7c5d38
th0r0nd0r <pjfritch1@gmail.com>
2018-01-15 13:12:53 -0800

    migratory birds

--- hackerrank/implementation.js
+++ hackerrank/implementation.js
@@ -1,3 +1,7 @@
+// given an array of n birds, whose types can be represented by the digits 1-5,
+// print the most common type of bird. If two types are equally common,
+// print the lesser digit
+
 function migratoryBirds(n, ar) {
     
 }
