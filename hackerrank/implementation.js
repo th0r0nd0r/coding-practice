98b79ef26c835f896f6c227c55b0da26c5c9ea49
th0r0nd0r <pjfritch1@gmail.com>
2018-01-15 13:36:37 -0800

    two characters

--- hackerrank/implementation.js
+++ hackerrank/implementation.js
@@ -3,15 +3,39 @@
 // print the lesser digit
 
 function migratoryBirds(n, ar) {
-  const counts = {
-      1: 0,
-      2: 0,
-      3: 0,
-      4: 0,
-      5: 0
-  }
+  const counts = [0,0,0,0,0];
+  let max = 0;
+  let maxIdx;
   
   for (let i = 0; i < ar.length; i++) {
-      
+      let type = ar[i];
+      counts[type - 1]++;
   }
+  
+  for (let i = 0; i < 5; i++) {
+      if (counts[i] > max) {
+          max = counts[i];
+          maxIdx = i;
+      }
+  }
+  
+  return maxIdx + 1;
+}
+
+
+// 
+
+function makeUnique(str) {
+  return String.prototype.concat(...new Set(str))
+}
+
+function twoCharaters(s) {
+    const chars = makeUnique(s);
+    
+    for (let i = 0; i < chars.length; i++) {
+        let regex = new RegExp(chars[i], "g");
+        let testT = s.replace(/)
+        let remainingChars = makeUnique()
+        while 
+    }
 }
