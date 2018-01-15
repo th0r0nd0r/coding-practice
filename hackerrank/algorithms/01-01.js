59dc68486552880186f0e0f134e9d11e74871a7f
th0r0nd0r <pjfritch1@gmail.com>
2018-01-15 14:53:54 -0800

    isPalindrome

--- hackerrank/algorithms/01-01.js
+++ hackerrank/algorithms/01-01.js
@@ -2,7 +2,7 @@ function isPalindrome(str) {
   let idx1 = 0;
   let idx2 = str.length - 1;
 
-  while (idx1 > idx2) {
+  while (idx1 < idx2) {
     if (str[idx1] === str[idx2]) {
       idx1++;
       idx2--;
