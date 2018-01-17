bf31b60b368b7956902f5031a6807c88d194743e
th0r0nd0r <pjfritch1@gmail.com>
2018-01-16 22:35:10 -0800

    digital root

--- hackerrank/algorithms/01-02.rb
+++ hackerrank/algorithms/01-02.rb
@@ -1,13 +1,13 @@
 def digital_root(n)
   root = 0
 
+  
   if n < 10
     return n
   end
 
   while n > 0
     root += n % 10
-    root += n / 10
     n /= 10
   end
 
@@ -17,3 +17,5 @@ end
 p digital_root(103)
 p digital_root(999)
 p digital_root(1234)
+
+3
