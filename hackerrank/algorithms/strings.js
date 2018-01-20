879ec0d6b747039a3d1c6a4d97daf1d027b368ba
th0r0nd0r <pjfritch1@gmail.com>
2018-01-19 20:47:46 -0800

    hackerrank in string

--- hackerrank/algorithms/strings.js
+++ hackerrank/algorithms/strings.js
@@ -9,3 +9,22 @@ function camelcase(s) {
   
   return words;
 }
+
+
+// return whether or not the given string contains "hackerrank" in order
+function hackerrankInString(s) {
+  const chars = ["h","a","c","k","e","r","r","a","n","k"];
+  let idx = 0;
+  
+  for (let i = 0; i < s.length; i++) {
+      if (s[i] === chars[idx]) {
+          idx++;
+      }
+  }
+  
+  if (idx === 10) {
+      return "YES";
+  } else {
+      return "NO";
+  }
+}
