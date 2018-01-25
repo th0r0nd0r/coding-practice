f2875c311d41440cb736b0d240c7d19cd9821d76
th0r0nd0r <pjfritch1@gmail.com>
2018-01-24 17:01:33 -0800

    two sum

---
+++ hackerrank/leetcode/easy.js
@@ -0,0 +1,11 @@
+var twoSum = function(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+      let first = nums[i];
+      for (let j = i + 1; j < nums.length; j++) {
+          let second = nums[j];
+          if (first + second === target) {
+              return [i, j];
+          }
+      }
+  }
+};
