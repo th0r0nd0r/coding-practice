210262329196e694f765e0f21124bbd32a6651a9
th0r0nd0r <pjfritch1@gmail.com>
2018-01-27 16:57:03 -0800

    utopian tree

--- hackerrank/implementation.js
+++ hackerrank/implementation.js
@@ -145,3 +145,18 @@ function cutTheSticks(arr) {
         }
     }
 }
+
+
+function utopianTree(n) {
+    let h = 1;
+    
+    for (let i = 0; i < n; i++) {
+        if (i % 2 === 0) {
+            h = h * 2;
+        } else {
+            h += 1;
+        }
+    }
+    
+    return h;
+}
