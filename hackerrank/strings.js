81eae826f22da4bd6fbbabf6bddf449615c0dd93
th0r0nd0r <pjfritch1@gmail.com>
2018-02-21 20:17:26 -0800

    mars exploration

--- hackerrank/strings.js
+++ hackerrank/strings.js
@@ -18,3 +18,26 @@ function isPangram(input) {
       console.log("not pangram");
   }
 }
+
+// determine how scrambled an sos message is
+function marsExploration(s) {
+    let scrambled = 0;
+    
+    let SIdx = 0;
+    let OIdx = 1;
+    
+    for (let i = 0; i < s.length; i++) {
+        if (i === OIdx && s[i] !== "O") {
+            scrambled++;
+        } else if ((i === SIdx || i === SIdx + 2) && s[i] !== "S") {
+            scrambled++;
+        }
+        
+        if (i === SIdx + 2) {
+            SIdx += 3;
+            OIdx += 3;
+        }
+    }
+    
+    return scrambled;
+}
