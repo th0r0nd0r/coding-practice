534159a7cb606e0d776fd4162c3eaa44a2cf9ba1
th0r0nd0r <pjfritch1@gmail.com>
2018-03-20 16:45:11 -0700

    bst check

--- hackerrank/data_structures/trees.py
+++ hackerrank/data_structures/trees.py
@@ -111,3 +111,16 @@ def decodeHuff(root , s):
             i += 1
         
     print message
+
+
+
+# determine if a binary tree is a bst
+def check_binary_search_tree_(root):
+    return checkBST(root, -1, 10001)
+
+def checkBST(root, Min, Max):
+    if not root:
+        return True
+    if root.data <= Min or root.data >= Max:
+        return False
+    return checkBST(root.left, Min, root.data) and checkBST(root.right, root.data, Max)
