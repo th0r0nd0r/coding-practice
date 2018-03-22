a4c3ab88a1a9cb80b8fd191a6bad20d459f1de4a
th0r0nd0r <pjfritch1@gmail.com>
2018-03-21 17:55:55 -0700

    simplified balanced brackets

--- hackerrank/data_structures/stacks.py
+++ hackerrank/data_structures/stacks.py
@@ -1,5 +1,5 @@
 # balanced brackets problem
-
+# verbose version because hackerranck doesn't want boolean answers
 def isBalanced(s):
     stack = []
     bracket_map = {
@@ -23,3 +23,20 @@ def isBalanced(s):
         return "YES"
     else:
         return "NO"
+
+# simplified version
+def isBalanced(s):
+    stack = []
+    bracket_map = {
+        "}":"{",
+        "]":"[",
+        ")":"("
+    }
+    
+    for b in s:
+        if b not in bracket_map:
+            stack.append(b)
+        elif not stack or bracket_map[b] != stack.pop():
+            return False
+    
+    return not stack
