6b4a4deac120359b8d61d0342e491689ec1f5e5a
th0r0nd0r <pjfritch1@gmail.com>
2018-03-29 15:47:18 -0700

    move tree traversal to medium

--- leetcode/easy.js
+++ leetcode/easy.js
@@ -106,28 +106,3 @@ var isValid = function(s) {
     }
 };
 
-// inorder binary tree traversal
-var inorderTraversal = function(root) {
-    const returnArr = [];
-    console.log(root);
-    
-    if (root) {
-        if (root.left) {
-            const left = inorderTraversal(root.left);
-            left.forEach(function(el) {
-                returnArr.push(el); 
-            });
-        }
-
-        returnArr.push(root.val);
-
-        if (root.right) {
-            const right = inorderTraversal(root.right);
-            right.forEach(function(el) {
-                returnArr.push(el); 
-            });
-        }
-    }
-
-    return returnArr;
-};
