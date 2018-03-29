bb9e68f02c78446f376180fee8aafa81e4744e8e
th0r0nd0r <pjfritch1@gmail.com>
2018-03-29 15:44:13 -0700

    inorder traversal

--- leetcode/easy.js
+++ leetcode/easy.js
@@ -105,3 +105,29 @@ var isValid = function(s) {
         return false;
     }
 };
+
+// inorder binary tree traversal
+var inorderTraversal = function(root) {
+    const returnArr = [];
+    console.log(root);
+    
+    if (root) {
+        if (root.left) {
+            const left = inorderTraversal(root.left);
+            left.forEach(function(el) {
+                returnArr.push(el); 
+            });
+        }
+
+        returnArr.push(root.val);
+
+        if (root.right) {
+            const right = inorderTraversal(root.right);
+            right.forEach(function(el) {
+                returnArr.push(el); 
+            });
+        }
+    }
+
+    return returnArr;
+};
