051836a139cde205ffb43eaafbacf7dd6768d190
th0r0nd0r <pjfritch1@gmail.com>
2018-03-30 12:37:06 -0700

    queensafe

--- leetcode/hard.js
+++ leetcode/hard.js
@@ -30,15 +30,26 @@ let board;
 
 // create this function inside n queens function so it has access to the 2d chess board
 function queenSafe(row, col) {
-    for (let i = 0; i < n; i++) {
-      let horizontal = board[row[i]];
-      let vertical = board[col[i]];
-      if (horizontal || vertical) {
-        return false;
-      }
+  for (let i = 0; i < n; i++) {
+    let horizontal = board[row[i]];
+    let vertical = board[col[i]];
+    if (horizontal || vertical) {
+      return false;
     }
+  }
 
-    for (let i = 0, j = 0; i < n && j < n; i++, j++) {
-      // check diagonal
+  const reset = Math.min(row,col);
+
+  for (let i = row - reset, j = col - reset; i < n && j < n; i++, j++) {
+    if (board[i][j]) {
+      return false;
     }
+  }
+  for (let i = row - reset, j = col + reset; i < n && j >= 0; i++, j--) {
+    if (board[i][j]) {
+      return false;
+    }
+  }
+
+  return true;
 }
