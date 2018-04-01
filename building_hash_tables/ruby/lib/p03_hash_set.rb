36d4f71bd8668aef832ca32e9d34596c172af797
th0r0nd0r <pjfritch1@gmail.com>
2018-04-01 11:51:55 -0700

    ruby hash set

--- building_hash_tables/ruby/lib/p03_hash_set.rb
+++ building_hash_tables/ruby/lib/p03_hash_set.rb
@@ -9,18 +9,31 @@ class HashSet
   end
 
   def insert(key)
+    num = key.hash
+
+    if @count >= num_buckets - 1
+      resize!
+    end
+
+    self[num] << key
+    @count += 1
   end
 
   def include?(key)
+    num = key.hash
+    self[num].include?(key)
   end
 
   def remove(key)
+    num = key.hash
+    self[num].delete(key)
+    @count -= 1
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +41,14 @@ class HashSet
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2) { Array.new }
+    @store.each do |bucket|
+      bucket.each do |key|
+        num = key.hash
+        new_store[num % (num_buckets * 2)] << key
+      end
+    end
+
+    @store = new_store
   end
 end
