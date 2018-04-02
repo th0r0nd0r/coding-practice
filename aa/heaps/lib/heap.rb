15963315015fe379122c6f7be7add30adf1fef98
th0r0nd0r <pjfritch1@gmail.com>
2018-04-02 11:34:30 -0700

    heap methods

--- aa/heaps/lib/heap.rb
+++ aa/heaps/lib/heap.rb
@@ -20,6 +20,10 @@ class BinaryMinHeap
   def push(val)
   end
 
+  def swap!(array, i1, i2)
+    array[i1], array[i2] = array[i2], array[i1]
+  end
+
   public
   def self.child_indices(len, parent_index)
     indices = []
@@ -34,11 +38,25 @@ class BinaryMinHeap
   end
 
   def self.parent_index(child_index)
+    if child_index == 0
+      raise "root has no parent"
+    end
+
+    (child_index - 1) / 2
   end
 
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
+    prc ||= Proc.new {|a, b| a <=> b}
+    
   end
-
+  
   def self.heapify_up(array, child_idx, len = array.length, &prc)
+    prc ||= Proc.new {|a, b| a <=> b}
+    parent_idx = parent_index(child_idx)
+    parent = array[parent_idx]
+    child = array[child_idx]
+
+    if prc.call(child, parent) >= 0
+
   end
 end
