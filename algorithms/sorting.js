d07323cb6656632e16ca65f98590bbca3fb446d9
th0r0nd0r <pjfritch1@gmail.com>
2018-04-03 14:56:19 -0700

    in place quicksort

--- algorithms/sorting.js
+++ algorithms/sorting.js
@@ -33,8 +33,6 @@ function quickSort(arr, comp) {
 // console.log(quickSort([-3,2.4,5.88, 1, -6], comparator));
 
 function swap(arr, i, j) {
-  console.log(arr, i, j);
-  console.log();
   const el1 = arr[i];
   const el2 = arr[j];
   arr[i] = el2;
@@ -46,40 +44,20 @@ function inPlaceQuicksort(arr, start, end, comp) {
     return arr;
   }
 
-  // const pivotIdx = start;
   const pivot = arr[start];
   let partition = start + 1;
-  console.log("start: ", start);
-  console.log("end: ", end);
-  console.log("pivot: ", pivot);
-  console.log("partition: ", partition);
-  console.log("sub-array: ", arr.slice(start, end + 1));
-  console.log();
 
   for (let i = start + 1; i <= end; i++) {
     let el = arr[i];
-    console.log("el: ", el);
-    console.log();
     if (comp(el, pivot) <= 0) {
       if (i !== start + 1) {
-        console.log("array: ", arr);
-        console.log("partition: ", partition);
-        console.log("i: ", i);
-        console.log("swapping....................");
-        console.log();
-
         swap(arr, partition, i);
-        console.log("array after swap: ", arr);
       }
       partition++;
     }
   }
 
   swap(arr, start, partition - 1);
-  console.log("left array: ", arr.slice(start, partition - 1));
-  console.log("pivot/partition: ", arr[partition - 1]);
-  console.log("right array: ", arr.slice(partition, end + 1));
-  console.log();
   inPlaceQuicksort(arr, start, partition - 2, comp);
   inPlaceQuicksort(arr, partition, end, comp);
 
@@ -87,8 +65,5 @@ function inPlaceQuicksort(arr, start, end, comp) {
 }
 
 console.log(inPlaceQuicksort([6,4,5,-2,8,3,9], 0, 6, comparator));
-// [6,4,5,-2,8,3,9]
-// [3,4,5,-2], 6, [8,9]
-// [-2] 3 [4,5]
-// console.log(inPlaceQuicksort([0,0,0,1,0], 0, 5, comparator));
-// console.log(inPlaceQuicksort([-3,2.4,5.88, 1, -6], 0, 5, comparator));
+console.log(inPlaceQuicksort([0,0,0,1,0], 0, 4, comparator));
+console.log(inPlaceQuicksort([-3,2.4,5.88, 1, -6], 0, 4, comparator));
