c36a0eef0dc046e77faefd6524c501b396329be8
th0r0nd0r <pjfritch1@gmail.com>
2018-04-03 14:06:59 -0700

    in place quicksort

--- algorithms/sorting.js
+++ algorithms/sorting.js
@@ -32,3 +32,47 @@ function quickSort(arr, comp) {
 // console.log(quickSort([0,0,0,1,0], comparator));
 // console.log(quickSort([-3,2.4,5.88, 1, -6], comparator));
 
+function swap(arr, i, j) {
+  // console.log(arr, i, j);
+  // console.log();
+  const el1 = arr[i];
+  const el2 = arr[j];
+  arr[i] = el2;
+  arr[j] = el1;
+}
+
+function inPlaceQuicksort(arr, start, end, comp) {
+  if ((end - start) < 2) {
+    return arr;
+  }
+
+  // const pivotIdx = start;
+  const pivot = arr[start];
+  let partition = start + 1;
+  console.log("start: ", start);
+  console.log("partition: ", partition);
+
+  for (let i = start + 1; i < end; i++) {
+    let el = arr[i];
+    if (comp(el, pivot) <= 0) {
+      if (i !== start + 1) {
+        console.log("array: ", arr);
+        console.log("partition: ", partition);
+        console.log("i: ", i);
+        console.log();
+        swap(arr, partition, i);
+      }
+      partition++;
+    }
+  }
+
+  swap(pivot, partition - 1);
+  inPlaceQuicksort(arr, start, partition - 1, comp);
+  inPlaceQuicksort(arr, partition + 1, end, comp);
+
+  return arr;
+}
+
+console.log(inPlaceQuicksort([2,4,5,8,3,9], 0, 6, comparator));
+// console.log(inPlaceQuicksort([0,0,0,1,0], 0, 5, comparator));
+// console.log(inPlaceQuicksort([-3,2.4,5.88, 1, -6], 0, 5, comparator));
--- notes/sorting.md
+++ notes/sorting.md
@@ -8,3 +8,21 @@
 1. select first element as pivot
 2. partition array around pivot
 3. recurse on left and right
+4. return concatted arrays
+
+#### In-place version (sublinear space complexity)
+- pass in the array, startIdx, and endIdx
+- base case/exit condition: 
+  + return arr if length <= 1
+1. select a pivot (first el)
+2. partition array in place around pivot
+  - assign barrier index after first element
+  - if  el < pivot
+    + swap el with first num after barrier
+    + increment barrier
+  - if el > pivot, don't do anything
+3. move pivot to spot directly left of barrier
+4. recurse on left and right sides
+  - sort left from 0...pivotIdx
+  - sort right from pivotIdx + 1...arr.length
+5. return array
