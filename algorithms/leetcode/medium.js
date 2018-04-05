ce160583b517d27cc2d1674f0f199d16480f2820
th0r0nd0r <pjfritch1@gmail.com>
2018-04-05 15:12:03 -0700

    reverse between

--- algorithms/leetcode/medium.js
+++ algorithms/leetcode/medium.js
@@ -77,3 +77,66 @@ var leastInterval = function(tasks, n) {
 };
 
 leastInterval(["A","A","A","B","B","B"], 2);
+
+// reverse a linked list between positions m and n (start indexing from 1 for some reason)
+// doesn't work yet
+var reverseBetween = function(head, m, n) {
+    if (head === null || head.next === null || m === n || m > n) {
+        return head;
+    }
+    
+    let i = 1;
+    let c = head;
+    let headCheck = head.next;
+    let p, h;
+    // let test = head;
+    
+    
+    // while (test !== null) {
+    //     console.log("test: ", test);
+    //     test = test.next;
+    // }
+    
+    while (i < m - 1) {
+        c = head.next;
+        i++;
+    }
+    let lastNonReversed = c;
+    let firstReversed = c.next;
+    if (i > 1 || m > 1) {
+        p = firstReversed;
+        c = p.next;
+    } else {
+        p = c;
+        c = p.next;
+    }
+    
+    h = c.next;
+    
+    // console.log("i: ", i);
+    if (i > 1 || m > 1) {
+        i++;
+    }
+    
+    while (i < n) {
+        // console.log("i: ", i);
+        // console.log("c: ", c);
+        c.next = p;
+        p = c;
+        c = h;
+        if (h !== null) {
+            h = h.next;   
+        }
+        i++;
+    }
+    
+    lastNonReversed.next = c;
+    if (c !== null && c.next !== null) {   
+        firstReversed.next = c.next;    
+    } 
+    if (headCheck === head.next) {
+        return head;   
+    } else {
+        return p;
+    }
+};
