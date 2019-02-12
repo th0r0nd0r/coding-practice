c89e6606444b14104322e2a4c3356a1ff143846f
th0r0nd0r <pjfritch1@gmail.com>
2019-02-11 17:19:09 -0800

    add info for electronics store

--- algorithms/hackerrank/algorithms/electronics-shop.js
+++ algorithms/hackerrank/algorithms/electronics-shop.js
@@ -1,4 +1,4 @@
-
+// basic O(n*m) solution. A more optimal solution involves sorting both lists and not checking combos greater than budget.
 
 function getMoneySpent(keyboards, drives, b) {
   let maxSkrilla = -1;
