a109a0bb2ab3980f0e159e94a24cf80fbcc86a84
th0r0nd0r <pjfritch1@gmail.com>
2019-02-14 16:10:05 -0800

    pass tests

--- algorithms/hackerrank/algorithms/apples_oranges.js
+++ algorithms/hackerrank/algorithms/apples_oranges.js
@@ -1,12 +1,12 @@
 function countApplesAndOranges(s, t, a, b, apples, oranges) {
   // console.log(s,t,a,b, apples, oranges);
 
-  let houseApples = { fruits: 0 };
-  let houseOranges = { fruits: 0 };
+  let houseApples = { fruits: 0, treeLocation: a };
+  let houseOranges = { fruits: 0, treeLocation: b };
 
   function doCount(fruits, fruitCount) {
       fruits.forEach(function (fruit) {
-          const fruitSpot = a + fruit;
+          const fruitSpot = fruitCount.treeLocation + fruit;
           if (s <= fruitSpot && t >= fruitSpot) {
               fruitCount.fruits += 1;
           }
@@ -20,8 +20,8 @@ function countApplesAndOranges(s, t, a, b, apples, oranges) {
   console.log(houseOranges.fruits);
 }
 
-countApplesAndOranges(7,11,5,15,[-2,2,1],[5,-6]);
+// countApplesAndOranges(7,11,5,15,[-2,2,1],[5,-6]);
 
-console.log();
+// console.log();
 
-countApplesAndOranges(2,3,1,5,[2],[-2]);
+// countApplesAndOranges(2,3,1,5,[2],[-2]);
