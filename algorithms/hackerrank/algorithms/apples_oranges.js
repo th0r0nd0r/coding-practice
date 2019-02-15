ba69269c0a9135a6b03efa9878df438380f3d342
th0r0nd0r <pjfritch1@gmail.com>
2019-02-14 16:04:09 -0800

    apples oranges

---
+++ algorithms/hackerrank/algorithms/apples_oranges.js
@@ -0,0 +1,23 @@
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+  // console.log(s,t,a,b, apples, oranges);
+
+  let houseApples = { fruits: 0 };
+  let houseOranges = { fruits: 0 };
+
+  function doCount(fruits, fruitCount) {
+      fruits.forEach(function (fruit) {
+          const fruitSpot = a + fruit;
+          if (s <= fruitSpot && t >= fruitSpot) {
+              fruitCount.fruits += 1;
+          }
+      });
+  }
+
+  doCount(apples, houseApples);
+  doCount(oranges, houseOranges);
+
+  console.log(houseApples.fruits);
+  console.log(houseOranges.fruits);
+}
+
+countApplesAndOranges(7,11,5,15,[-2,2,1],[5,-6]);
