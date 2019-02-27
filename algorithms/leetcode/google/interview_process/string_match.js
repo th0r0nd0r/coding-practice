6dbc24d181672ad254070028f7b0e9887148c8ed
th0r0nd0r <pjfritch1@gmail.com>
2019-02-26 17:55:14 -0800

    long enough

--- algorithms/leetcode/google/interview_process/string_match.js
+++ algorithms/leetcode/google/interview_process/string_match.js
@@ -22,6 +22,24 @@ const a = 'dafsabcddsw';
 const b = 'abcd';
 const c = 'fsac';
 
-console.log(isSubstring(a,b));
-console.log(isSubstring(a,c));
-console.log(isSubstring(b, a));
+// console.log(isSubstring(a,b));
+// console.log(isSubstring(a,c));
+// console.log(isSubstring(b, a));
+
+function longEnough(A, B) {
+  for (let i = A.length - 1; i >= 0; i--) {
+      if (A[i] === B[0]) {
+          if (A.slice(i).length >= B.length) {
+              return true;
+          } else {
+              return false;
+          }
+      }
+  }
+  
+  return true;
+}
+
+const A = 'sdfs';
+const B = 'asd';
+console.log(longEnough(A, B));
