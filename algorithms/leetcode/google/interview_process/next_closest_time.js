c20db303da992fa4e7b858af8661292e7f123573
th0r0nd0r <pjfritch1@gmail.com>
2019-03-03 14:11:12 -0800

    next closest time

--- algorithms/leetcode/google/interview_process/next_closest_time.js
+++ algorithms/leetcode/google/interview_process/next_closest_time.js
@@ -7,34 +7,63 @@
 // - first goes 0-2, but only change if 2nd digit has decreased
  
 var nextClosestTime = function(time) {
-  const digits = [];
-  for (let i = 0; i < time.length; i++) {
-      const num = parseInt(time[i]);
-      if (num.toString() === time[i]) {
-          digits.push(num);
+  const strippedTime = time.replace(':','');
+  const nums = time.slice(0,2) + time.slice(3,5);
+  const digits = nums.split('').map(dig => parseInt(dig, 10));
+  const sorted = digits.sort();
+  const newDigits = [];
+  let finished = false;
+  
+  for (let i = 3; i >= 0; i--) {
+      const num = parseInt(strippedTime[i]);
+      
+      if (finished) {
+          newDigits.unshift(num);
+      } else {
+          let cap = 9;
+          switch (i) {
+              case 2:
+                  cap = 5;
+                  break;
+              case 1:
+                  if (strippedTime[0] === '2') {
+                      cap = 3;
+                  }
+                  break;
+              case 0:
+                  cap = 2;
+                  break;
+          }
+
+          for (let j = 0; j < sorted.length; j++) {
+              const dig = sorted[j];
+              
+              if (dig > num) {
+                  if (dig <= cap) {
+                      newDigits.unshift(dig);
+                      finished = true;
+                      break;
+                  } else {
+                      newDigits.unshift(sorted[0]);
+                      break;
+                  }
+              } else if (j === 3) {
+                  newDigits.unshift(sorted[0]);
+                  break;
+              }
+          }
       }
+      
   }
   
+  const newTime = newDigits.reduce((acc, curr, idx) => {
+      let newDig = curr.toString();
+      if (idx === 1) {
+          newDig += ':';
+      }
+      
+      return acc + newDig;
+  }, '');
+  
+  return newTime;
 };
-
-// var nextClosestTime = function(time) {
-//     const nums = time.slice(0,2) + time.slice(3,5);
-//     const digits parseInt(nums.split(''));
-//     const sorted = digits.slice().sort();
-//     let newTime = time;
-    
-//     digits.forEach(function(dig, i, digs)) {
-//         sorted.forEach(num, j, arr) {
-//             if (sorted[j] > digits[i]) {
-//                 if (i === 3) {
-//                     if  (sorted[j] < 6) {
-//                         newTime[4] = sorted[j]
-//                     }
-//                 }
-//             }
-//         }
-//     }
-
-//     return newTime;
-            
-// };
