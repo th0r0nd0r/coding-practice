07c8404b7254f0d3e935e373a2adaef6216dc757
th0r0nd0r <pjfritch1@gmail.com>
2019-03-03 13:04:17 -0800

    min stack

--- algorithms/leetcode/google/interview_process/next_closest_time.js
+++ algorithms/leetcode/google/interview_process/next_closest_time.js
@@ -5,7 +5,7 @@
 // - if first digit is 0 or 1: 2nd goes 0-9
 // - if first digit is 2: 2nd goes 0-3
 // - first goes 0-2, but only change if 2nd digit has decreased
-
+ 
 var nextClosestTime = function(time) {
   const digits = [];
   for (let i = 0; i < time.length; i++) {
@@ -16,3 +16,25 @@ var nextClosestTime = function(time) {
   }
   
 };
+
+// var nextClosestTime = function(time) {
+//     const nums = time.slice(0,2) + time.slice(3,5);
+//     const digits parseInt(nums.split(''));
+//     const sorted = digits.slice().sort();
+//     let newTime = time;
+    
+//     digits.forEach(function(dig, i, digs)) {
+//         sorted.forEach(num, j, arr) {
+//             if (sorted[j] > digits[i]) {
+//                 if (i === 3) {
+//                     if  (sorted[j] < 6) {
+//                         newTime[4] = sorted[j]
+//                     }
+//                 }
+//             }
+//         }
+//     }
+
+//     return newTime;
+            
+// };
