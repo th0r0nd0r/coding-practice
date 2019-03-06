750a2c9907302664f07200b50454e0bb5ddc5746
th0r0nd0r <pjfritch1@gmail.com>
2019-03-06 13:35:49 -0800

    graphs

---
+++ notes/data_structures/examples/graphs.js
@@ -0,0 +1,29 @@
+class UnweightedGraphNode {
+  constructor(data) {
+    this.data = data || null;
+  }
+}
+
+class UnweightedGraph {
+  constructor() {
+    this.vertexList = [];
+    this.adjList = [];
+    this.nodes = 
+  }
+
+  addVertex(data, connections) {
+
+  }
+
+  deleteVertex() {
+
+  }
+
+  findAdjacentNodes(node) {
+
+  }
+
+  areConnected(n1, n2) {
+
+  }
+ }
