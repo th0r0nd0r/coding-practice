e63d8c15672233414e9e92ee7e66a2382a3d9f64
th0r0nd0r <pjfritch1@gmail.com>
2019-03-06 14:05:46 -0800

    delete method

--- notes/data_structures/examples/graphs.js
+++ notes/data_structures/examples/graphs.js
@@ -1,22 +1,37 @@
-class UnweightedGraphNode {
+class GraphNode {
   constructor(data) {
     this.data = data || null;
   }
 }
 
-class UnweightedGraph {
+class Graph {
   constructor() {
     this.vertices = [];
     this.adjList = [];
   }
 
   addVertex(data, connections) {
-    const vertex = new UnweightedGraphNode(data);
-    this.vertices.push(vertex);
+    if (connections instanceof Array) {
+      const vertex = new GraphNode(data);
+      this.vertices.push(vertex);
+      this.adjList.push(connections);
+      connections.forEach((nodeIdx) => {
+        this.adjList[nodeIdx].push(this.vertices[this.vertices.length - 1]);
+      });
+    } else {
+      throw "connections must be an array of integers";
+    }
   }
 
-  deleteVertex() {
-
+  deleteVertex(nodeIdx) {
+    const vertex = this.vertices[nodeIdx];
+    const adjRow = this.adjList[nodeIdx];
+    adjRow.forEach((vertexIdx, i) => {
+      const adjVertex = this.adjList[vertexIdx];
+      this.adjList[vertexIdx] = adjVertex.filter(n => n !== nodeIdx);
+    });
+    this.vertices[nodeIdx] = null;
+    this.adjList[nodeIdx] = [];
   }
 
   adjacenttNodes(node) {
