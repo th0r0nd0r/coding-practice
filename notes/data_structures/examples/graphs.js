117a47d4354f082763cf2d35de1f3f8310b9a799
th0r0nd0r <pjfritch1@gmail.com>
2019-03-12 13:31:01 -0700

    implement recursive DFS

--- notes/data_structures/examples/graphs.js
+++ notes/data_structures/examples/graphs.js
@@ -1,3 +1,6 @@
+// this graph is undirected
+// the methods on the class assume that the graph is not empty and that there are no unconnected vertices
+
 class GraphNode {
   constructor(data) {
     this.data = data || null;
@@ -10,6 +13,8 @@ class Graph {
     this.adjList = [];
   }
 
+  // feed this fn the data for the new node, and an array of the node #s to connect to
+  // maybe a better way to implement would be to either pass an array of node.data, or have each GraphNode store it's vertex # internally
   addVertex(data, connections) {
     if (connections instanceof Array) {
       const vertex = new GraphNode(data);
@@ -45,23 +50,51 @@ class Graph {
   data(nodeIdx) {
     return this.vertices[nodeIdx].data;
   }
- }
 
- function getRandomInt(max) {
-   return Math.floor(Math.random() * Math.floor(max));
- }
+  recursiveDFS(value) {
+    const visited = new Set();
+    const startNode = this.vertices[0];
 
- const testGraph = new Graph();
+    const traverseChildren = (vIdx) => {
+      visited.add(vIdx);
+      const children = this.adjList[vIdx];
+      for (let i = 0; i < children.length; i++) {
+        const childIdx = children[i];
+        if (!visited.has(childIdx)) {
+          if (this.vertices[childIdx].data === value) {
+            return childIdx;
+          } else {
+            visited.add(childIdx);
+            return traverseChildren(childIdx);
+          }
+        }
+      }
+
+      return -1;
+    };
+
+    return traverseChildren(0);
+  }
+ }
  
- const nodeData = [3,"hello",['a','b','c'],{3:"something"},43.5];
- nodeData.forEach((data, i) => {
-   const connections = [];
-  [...Array(i).keys()].forEach(() => {
-    connections.push(getRandomInt(i - 1));
-  });
 
-  testGraph.addVertex(data,connections);
- });
 
- console.log("vertices: ", testGraph.vertices);
- console.log("adjacency list: ", testGraph.adjList);
+
+//  function getRandomInt(max) {
+//    return Math.floor(Math.random() * Math.floor(max));
+//  }
+
+//  const testGraph = new Graph();
+ 
+//  const nodeData = [3,"hello",['a','b','c'],{3:"something"},43.5];
+//  nodeData.forEach((data, i) => {
+//    const connections = [];
+//   [...Array(i).keys()].forEach(() => {
+//     connections.push(getRandomInt(i - 1));
+//   });
+
+//   testGraph.addVertex(data,connections);
+//  });
+
+//  console.log("vertices: ", testGraph.vertices);
+//  console.log("adjacency list: ", testGraph.adjList);
