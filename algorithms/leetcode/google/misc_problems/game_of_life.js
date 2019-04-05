cc78c594284510ede31e30ae47fa0a87c70ebf1a
th0r0nd0r <pjfritch1@gmail.com>
2019-04-05 11:13:44 -0700

    game of life

--- algorithms/leetcode/google/misc_problems/game_of_life.js
+++ algorithms/leetcode/google/misc_problems/game_of_life.js
@@ -5,21 +5,37 @@ const board = [
 [0,0,0]
 ];
 
+function nextValue(board, i, j) {
+    const m = board.length;
+    const n = board[0].length;
+    const maxA = i + 1 < m ? i + 2 : m;
+    const maxB = j + 1 < n ? j + 2 : n;
+    
+    for (let a = i > 0 ? i - 1 : 0; a < maxA; a++) {
+        for (let b = j > 0 ? j - 1 : 0; b < maxB; b++) {
+            if (a === i && b === j) {
+                continue;
+            }
+            
+            
+        }
+    }
+}
+
 var gameOfLife = function(board) {
-  const nextBoard = [];
-  for (let i = 0; i < board.length; i++) {
-      board.push(new Array(board[0].length));
-  }
-  console.log(nextBoard);
-  
-  for (let i = 0; i < board.length; i++) {
-      for (let j = 0; j < board[0].length; j++) {
-          // let neighbors = 0;
-//             check all adjacent blocks
-      }
-  }
-  
-  return nextBoard;
+    const nextBoard = [];
+    for (let i = 0; i < board.length; i++) {
+        nextBoard.push(new Array(board[0].length));
+    }
+    console.log(nextBoard);
+    
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[0].length; j++) {
+            nextBoard[i][j] = nextValue(board, i, j);
+        }
+    }
+    
+    return nextBoard;
 };
 
-
+gameOfLife(board);
