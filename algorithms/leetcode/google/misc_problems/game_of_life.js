1182479a04b391c22f07ce6d7465834988b41013
th0r0nd0r <pjfritch1@gmail.com>
2019-04-05 11:32:08 -0700

    game of life not in-place

--- algorithms/leetcode/google/misc_problems/game_of_life.js
+++ algorithms/leetcode/google/misc_problems/game_of_life.js
@@ -11,15 +11,32 @@ function nextValue(board, i, j) {
     const maxA = i + 1 < m ? i + 2 : m;
     const maxB = j + 1 < n ? j + 2 : n;
     
+    let numNeighbors = 0;
+    
     for (let a = i > 0 ? i - 1 : 0; a < maxA; a++) {
         for (let b = j > 0 ? j - 1 : 0; b < maxB; b++) {
             if (a === i && b === j) {
                 continue;
             }
-            
-            
+            if (board[a][b] === 1) {
+                numNeighbors++;
+            }
         }
     }
+    
+    let newValue = board[i][j];
+    
+    if (board[i][j] === 0) {
+        if (numNeighbors === 3) {
+            newValue = 1;
+        }
+    } else {
+        if (numNeighbors > 3 || numNeighbors < 2) {
+            newValue = 0;
+        }
+    }
+    
+    return newValue;
 }
 
 var gameOfLife = function(board) {
@@ -27,14 +44,14 @@ var gameOfLife = function(board) {
     for (let i = 0; i < board.length; i++) {
         nextBoard.push(new Array(board[0].length));
     }
-    console.log(nextBoard);
     
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[0].length; j++) {
-            nextBoard[i][j] = nextValue(board, i, j);
+            const newValue = nextValue(board, i, j);
+            nextBoard[i][j] = newValue;
         }
     }
-    
+    console.log(nextBoard);
     return nextBoard;
 };
 
