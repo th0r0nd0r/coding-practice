1e2d118d453ef3c1dbf09c324132e732c3b37b20
th0r0nd0r <pjfritch1@gmail.com>
2019-04-13 10:24:43 -0700

    permutation palindromes

--- algorithms/interview_cake/hashes.js
+++ algorithms/interview_cake/hashes.js
@@ -12,3 +12,55 @@ function compatibleMovies(flightTime, movieTimes) {
 
   return false;
 }
+
+// console.log(compatibleMovies(8, [4,3,9,1,2]));
+// console.log(compatibleMovies(8, [4,3,9,1,5,2]));
+
+// same but with tolerance window (movie ends within x minutes of flight landing)
+// function compatibleMovies2(flightTime, movieTimes, window) {
+//   const times = new Set();
+
+//   for (let i = 0; i < movieTimes.length; i++) {
+//     if (times.has(flightTime - movieTimes[i])) {
+//       return true;
+//     }
+//     times.add(movieTimes[i]);
+//   }
+
+//   return false;
+// }
+
+
+
+// find if any permutation of a string is a palindrome
+function permutationPalindrome(str) {
+  const charCounts = {};
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (charCounts[char]) {
+      charCounts[char]++;
+    } else {
+      charCounts[char] = 1;
+    }
+  }
+
+  const counts = Object.values(charCounts);
+  let odds = 0;
+
+  for (let i = 0; i < counts.length; i++) {
+    if (counts[i] % 2 !== 0) {
+      odds++;
+      if (odds > 1) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+console.log(permutationPalindrome("ivicc"));
+console.log(permutationPalindrome("civil"));
+console.log(permutationPalindrome("livci"));
