28b1a7baeba97dc3f4f3a90b78740660c3586d11
th0r0nd0r <pjfritch1@gmail.com>
2019-04-15 13:20:56 -0700

    make bank

---
+++ algorithms/interview_cake/greedy.js
@@ -0,0 +1,25 @@
+// input: an array of stock prices whose indices correspond to minutes since open
+// output: the most $$ I could make from one buy + one sell
+function makeMeRich(prices) {
+  let lowest = prices[0];
+  let maxDiff = prices[1] - lowest;
+
+  for (let i = 1; i < prices.length; i++) {
+    const price = prices[i];
+
+    if (price < lowest) {
+      lowest = price;
+    } else {
+      const diff = price - lowest;
+      if (diff > maxDiff) {
+        maxDiff = diff;
+      }
+    }
+  }
+
+  return maxDiff;
+}
+
+console.log(makeMeRich([10, 7, 5, 8, 11, 9]));
+console.log(makeMeRich([10, 12, 5, 3, 8, 9]));
+console.log(makeMeRich([8,2,4,5]));
