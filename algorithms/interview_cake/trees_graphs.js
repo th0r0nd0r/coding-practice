0be71b5aaeca3f8d090143225341514b121c9112
th0r0nd0r <pjfritch1@gmail.com>
2019-04-22 18:18:50 -0700

    superbalanced

--- algorithms/interview_cake/trees_graphs.js
+++ algorithms/interview_cake/trees_graphs.js
@@ -17,5 +17,30 @@ class BinaryTreeNode {
 
 // determine if the max difference between lengths of binary tree branches is <= 1
 function isSuperBalanced(baseNode) {
-  
+  let maxHeight = 0, minHeight = 0;
+  let currHeight = 0;
+  // the stack also stores the height of each node
+  const stack = [{node: baseNode, height: 0}];
+
+  while (stack.length) {
+    if (maxHeight - minHeight > 1) {return false;
+    }
+    const nodeObj = stack.pop();
+    currHeight = nodeObj.height;
+    const node = nodeObj.node;
+
+    if (!node.left && !node.right) {
+      if (currHeight > maxHeight) {
+        maxHeight = currHeight;
+      }
+      if (currHeight < minHeight) {
+        minHeight = currHeight;
+      }
+    } else {
+      if (node.right) {stack.push({node: node.right, height: currHeight + 1});}
+      if (node.left) {stack.push({node: node.left, height: currHeight + 1});}
+    }
+  }
+
+  return (maxHeight - minHeight <= 1);
 }
